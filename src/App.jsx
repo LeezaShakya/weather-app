a8eab5fe5f4d1606e9aa7e2265ab245e7cb9192a
Leeza@218 <leezashakya218@gmail.com>
2024-08-07 17:55:35 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Weather } from './components/weather'
+
+function App() {
+
+  return (
+    <>
+      <div className='h-screen flex justify-center items-center text-white bg-slate-900 '>
+      <Weather/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/weather.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Weather = () => {
+  return (
+    <div className='bg-white text-black'>
+        
+        <div className='search-bar'>
+            <input type="text" />
+        </div>
+    </div>
+  )
+}
